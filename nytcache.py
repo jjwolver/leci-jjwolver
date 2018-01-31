7da99a1474119aa5a80a626f981d020342662214
Jeremy Wolverton <jjwolver@umich.edu>
2018-01-30 20:18:24 -0500

    modified nytcache file

--- nytcache.py
+++ nytcache.py
@@ -1,7 +1,7 @@
 #ny-simple.py
 from secrets import *
 import requests
-
+from datetime import datetime
 
 #datamuse-caching
 import requests
@@ -29,6 +29,12 @@ def params_unique_combination(baseurl, params):
         res.append("{}-{}".format(k, params[k]))
     return baseurl + "_".join(res)
 
+MAX_STALENESS = 30 ## 30 seconds--only for lecture demo!
+def is_fresh(cache_entry):
+    now = datetime.now().timestamp()
+    staleness = now - cache_entry['cache_timestamp']
+    return staleness > MAX_STALENESS
+
 # The main cache function: it will always return the result for this
 # url+params combo. However, it will first look to see if we have already
 # cached the result and, if so, return the result from cache.
@@ -38,21 +44,26 @@ def make_request_using_cache(baseurl, params):
 
     ## first, look in the cache to see if we already have this data
     if unique_ident in CACHE_DICTION:
-        print("Getting cached data...")
-        return CACHE_DICTION[unique_ident]
+        if is_fresh(CACHE_DICTION[unique_ident]): #### THIS IS NEW
+            print("Getting cached data...")
+            return CACHE_DICTION[unique_ident]
+    else:
+        pass ## it's a cache miss, fall through to refresh code
 
     ## if not, fetch the data afresh, add it to the cache,
     ## then write the cache to file
-    else:
-        print("Making a request for new data...")
-        # Make the request and cache the new data
-        resp = requests.get(baseurl, params)
-        CACHE_DICTION[unique_ident] = json.loads(resp.text)
-        dumped_json_cache = json.dumps(CACHE_DICTION)
-        fw = open(CACHE_FNAME,"w")
-        fw.write(dumped_json_cache)
-        fw.close() # Close the open file
-        return CACHE_DICTION[unique_ident]
+
+    print("Making a request for new data...")
+    # Make the request and cache the new data
+    resp = requests.get(baseurl, params)
+    CACHE_DICTION[unique_ident] = json.loads(resp.text)
+    ### THE NEXT LINE IS NEW
+    CACHE_DICTION[unique_ident]['cache_timestamp'] = datetime.now().timestamp()
+    dumped_json_cache = json.dumps(CACHE_DICTION)
+    fw = open(CACHE_FNAME,"w")
+    fw.write(dumped_json_cache)
+    fw.close() # Close the open file
+    return CACHE_DICTION[unique_ident]
 
 
 # gets stories from a particular section of NY times
@@ -69,7 +80,7 @@ def get_headlines(nyt_results_dict):
         headlines.append(r['title'])
     return headlines
 
-story_list_json = get_stories('science')
+story_list_json = get_stories('technology')
 headlines = get_headlines(story_list_json)
 for h in headlines:
     print(h)
